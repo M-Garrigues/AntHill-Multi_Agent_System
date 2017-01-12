d4cc612c23197c184c06b4f4834557442eb414f2
Mathieu Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2017-01-12 08:41:48 +0100

    Enum ElementTypes

---
+++ src/model/elements/ElementTypes.java
@@ -0,0 +1,14 @@
+package model.elements;
+
+/**
+ * Coded by Mathieu GARRIGUES on 12/01/2017.
+ */
+public enum ElementTypes {
+
+    AntHill,
+    Obstacle,
+    Pheromone,
+    Source
+    //Ant;
+
+}
--- src/model/map/Cell.java
+++ src/model/map/Cell.java
@@ -14,7 +14,7 @@ public class Cell {
 
 
 
-    private ElementList elements;
+    private Map<> elements;
     private Position position;
 
 
