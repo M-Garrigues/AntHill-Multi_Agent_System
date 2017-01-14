8fa7a151dcad8638be44e41e11df8aee31e72358
Florian <florian.begonin@gmail.com>
2017-01-14 18:20:17 +0100

    Class OneStep implements Movement

--- src/model/agents/mobileAgent/movement/OneStep.java
+++ src/model/agents/mobileAgent/movement/OneStep.java
@@ -1,7 +1,32 @@
 package model.agents.mobileAgent.movement;
 
+import model.Position;
+import model.map.Cell;
+import model.map.Map;
+
+import java.util.ArrayList;
+
 /**
  * Created by Florian on 13/01/2017.
  */
-public class OneStep {
+public class OneStep implements Movement{
+
+    public ArrayList move(Map map, Position pos){
+        ArrayList<Cell> cellMove = new ArrayList<>();
+
+        int moveX = pos.getX() - 1;
+        int moveY = pos.getY() -1;
+        Position posTemp = new Position();
+
+        for(int i = moveX; i < moveX + 3; i++){ //This double for adds a square of 9 cells to viewedCells, with the given position as the centre.
+            for(int j = moveY; j < moveY; j++){
+                posTemp.setX(i);
+                posTemp.setY(j);
+                if (!map.getCellPosition(posTemp).isObstacle()) {
+                    cellMove.add(map.getCellPosition(posTemp));
+                }
+            }
+        }
+        return cellMove;
+    }
 }
