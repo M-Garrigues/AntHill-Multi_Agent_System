fee71f367b8dd44f55dd156ca15c746e167c607d
Florian <florian.begonin@gmail.com>
2017-01-28 16:46:36 +0100

    fetch \initialization

--- src/model/agents/behaviour/Fetch.java
+++ src/model/agents/behaviour/Fetch.java
@@ -3,6 +3,8 @@ package model.agents.behaviour;
 import model.Position;
 import model.agents.Agent;
 import model.agents.mobileAgent.ant.Ant;
+import model.elements.Element;
+import model.elements.Pheromone;
 import model.map.Cell;
 import view.ErrorView;
 
@@ -26,6 +28,7 @@ public class Fetch implements Behaviour {
 
         ArrayList<Cell> movableCell = ant.getMoveCells();
         ArrayList<Cell> path = ant.getPath();
+
         ArrayList<Double> probabilityArray = new ArrayList<Double>();
 
         Cell lastCell = path.get(path.size() - 1);
@@ -33,50 +36,88 @@ public class Fetch implements Behaviour {
         Cell nextCell = agent.getMap().nextCell(lastCell,actualCell); //Cell in front of the ant
         Cell endCell = new Cell();
 
-
-        for (int i = 0 ; i < path.size() ; i++){
-            if (movableCell.contains(path.get(i))){
-                movableCell.remove(path.get(i));
-            }
-        }
-
-        // Probability for each possibleCell
-        for (int i = 0 ; i < movableCell.size() ; i++){
-            double probability;
-            int distance;
-            Cell cellCompare = movableCell.get(i);
-            Position positionCompare = cellCompare.getPosition();
-            if (nextCell.getPosition().isEqual(positionCompare)){
-                probability = 10;
-            }
-            else {
-                distance = nextCell.distance(cellCompare);
-                probability = 10 / (distance + 1);
-            }
-
-            probabilityArray.add(probability);
-        }
-
-        //Sum for all probability
-        double sumProbability = 0;
-
-        for (int i = 0 ; i < probabilityArray.size() ; i++){
-            sumProbability += probabilityArray.get(i);
-        }
-
-        //Generating random number to chose next cell
-        double testProbability = Math.random();
-        double cumulSum = 0;
-        for (int i = 0; i < probabilityArray.size() ; i++){
-
-            cumulSum += probabilityArray.get(i)/sumProbability;
-            if (testProbability <= cumulSum){
+        Boolean source = false;
+        for (int i = 0 ; i < movableCell.size(); i++){
+            if (movableCell.get(i).isSource()){
+                source = true;
                 endCell = movableCell.get(i);
             }
         }
+        if (source == true){
+            ant.getPath().add(endCell);
+            ant.move(actualCell, endCell);
 
-        ant.move(actualCell,endCell);
+            //Change behaviour to getFood
+        }
+        else {
 
+            //Choose a new cell
+
+            for (int i = 0; i < path.size(); i++) {
+                if (movableCell.contains(path.get(i))) {
+                    movableCell.remove(path.get(i));
+                    //Ant don't come back on her path
+                }
+            }
+            if (movableCell.isEmpty()) {
+
+                ant.getPath().add(lastCell);
+                ant.move(actualCell, lastCell);
+                //If there is no possibility to move , go back
+            } else {
+
+                // Probability for each possibleCell
+                for (int i = 0; i < movableCell.size(); i++) {
+
+                    double probability;
+                    int distance;
+
+                    Cell cellCompare = movableCell.get(i);
+
+                    Position positionCompare = cellCompare.getPosition();
+                    Element pheromoneElement = cellCompare.getPheromone();
+
+                    Pheromone pheromone = (Pheromone) pheromoneElement;
+
+                    if (nextCell.getPosition().isEqual(positionCompare)) {
+                        probability = 10; //orientation
+                        probability += pheromone.getQuantity(); //pheromone
+
+                    } else {
+                        distance = nextCell.distance(cellCompare);
+                        probability = 10 / (distance + 1); //orientation
+                        probability += pheromone.getQuantity(); //pheromone
+                    }
+
+                    probabilityArray.add(probability);
+                }
+
+                //Sum for all probability
+                double sumProbability = 0;
+
+                for (int i = 0; i < probabilityArray.size(); i++) {
+
+                    sumProbability += probabilityArray.get(i);
+                }
+
+                //Generating random number to chose next cell
+                double testProbability = Math.random();
+                double cumulSum = 0;
+
+                for (int i = 0; i < probabilityArray.size(); i++) {
+
+                    cumulSum += probabilityArray.get(i) / sumProbability;
+
+                    if (testProbability <= cumulSum) {
+
+                        endCell = movableCell.get(i);
+                    }
+                }
+
+                ant.getPath().add(endCell);
+                ant.move(actualCell, endCell);
+            }
+        }
 
     }
 }
