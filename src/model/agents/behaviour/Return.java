e049b23488ebfa148d340732bb2c26322c19a587
Florian <florian.begonin@gmail.com>
2017-01-29 14:57:46 +0100

    return ok

--- src/model/agents/behaviour/Return.java
+++ src/model/agents/behaviour/Return.java
@@ -42,31 +42,40 @@ public class Return implements Behaviour {
             ArrayList<Cell> path = ant.getPath();
             int i = 0;
             boolean newPath = true;
-
+            /*
             while ((i < path.size()) && newPath) {
+                System.out.println(newPath);
                 for (int j = 0; j < movableCells.size(); j++) {
 
                     Cell cellPath = path.get(i);
                     Cell cellMove = path.get(j);
                     if (cellPath.getPosition().isEqual(cellMove.getPosition())) {
                         System.out.println("je passe ici");
-
                         endCell = cellPath;
                         newPath = false;
                     }
                 }
                 i++;
-            }
+            }*/
 
-            if(!startCell.hasPheromone()){
+            endCell= path.get(path.size()-1);
+            ant.getPath().remove(path.size()-1);
+            System.out.println(startCell.hasPheromone());
+            if(!startCell.hasPheromone() && ant.hasFood()){
+                System.out.println("Je pose des phéromones");
                 Pheromone pheromone = new Pheromone(startCell.getPosition(), 10);
                 startCell.addElement(pheromone);
             }
-            else{
+            else if(startCell.hasPheromone() && ant.hasFood()) {
+                System.out.println("Je pose des phéromones");
                 int quantityPheromone = ((Pheromone)startCell.getPheromone()).getQuantity();
                 startCell.setPheromone(quantityPheromone + 10);
             }
+            else{
+                System.out.println("Je ne pose pas de phéromone parceque je suis un déchet");
+            }
 
+            System.out.println("Prochaine cellule : "+endCell.getPosition().getX()+" ; "+endCell.getPosition().getY());
             ant.move(startCell, endCell);
 
             if(endCell.isAntHill()){  //The agent is on an anthill, his state goes to dropFood.
